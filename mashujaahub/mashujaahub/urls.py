4512c46ea6e1149d09976ebf5752e0b321a54a3a
David PuronZ <dpuron@barbaraiot.com>
2020-01-18 10:29:54 +0100

    Adding right site name, templates

--- mashujaahub/mashujaahub/urls.py
+++ mashujaahub/mashujaahub/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from registration import views
 
+admin.site.site_header = "Mashujaa Hub Admin"
+admin.site.site_title = "Mashujaa Hub Admin"
+admin.site.index_title = "Mashujaa Hub Admin"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
