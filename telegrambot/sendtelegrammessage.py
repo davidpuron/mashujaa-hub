9221ee5ce6e69fe7cdf20ad3e7d3067c7eadb2af
David PuronZ <dpuron@barbaraiot.com>
2020-02-01 11:09:49 +0100

    Checking chat id from database

--- telegrambot/sendtelegrammessage.py
+++ telegrambot/sendtelegrammessage.py
@@ -1,6 +1,7 @@
 import requests
 import mashujaahub
 import os
+from registration.models import Artisan
 
 path = os.path.dirname(mashujaahub.__file__)
 fo = open(path+"/.token","r")
@@ -8,13 +9,11 @@ bot_token = fo.readline().rstrip('\n')
 fo.close()
 
 def telegram_bot_getchatid(phone):
-    status_update = 'https://api.telegram.org/bot'+bot_token+'/getUpdates'
-    response = requests.get(status_update).json()
-    for message in response['result']:
-        if message['message']['text'] == phone:
-            return message['message']['chat']['id']
-    return -1
-
+    try:
+        obj = Artisan.objects.get(phone=phone)
+        return obj.telegramChatId
+    except Artisan.DoesNotExist:
+        return -1
 
 def telegram_bot_sendtext(bot_message, bot_chatID):
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + str(bot_chatID) + '&parse_mode=Markdown&text=' + bot_message
