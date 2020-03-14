97dce4314ed9b29f59d999e5f26ce02743b3b89e
David PuronZ <dpuron@barbaraiot.com>
2020-03-14 10:22:53 +0100

    Adding import/export functionality

--- registration/admin.py
+++ registration/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from registration.models import Artisan
 from django.contrib.auth.models import Group,User
+from import_export.admin import ImportExportModelAdmin
 
-class ArtisanAdmin(admin.ModelAdmin):
+class ArtisanAdmin(ImportExportModelAdmin):
     list_display=('name','type','phone')
     list_filter=('type',)
 
